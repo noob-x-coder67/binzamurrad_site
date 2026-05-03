d9de4aebe010561f27de680ffe422ca434858fe6
noob-x-coder67 <mazzkhanmastoi786@gmail.com>
2026-05-03 14:25:22 +0500

    added analytics  Info

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,12 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <Analytics />
     </>
   );
 }
